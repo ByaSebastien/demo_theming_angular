d5ad4c90e9bf56b698df9bc3750e566a5eead7e3
Bya Sébastien <sebastien.bya@bstorm.be>
2025-08-28 15:03:55 +0200

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, HostBinding} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'demo_theming';
+
+  @HostBinding('class') className = '';
+
+  isDark = false;
+
+  toggleTheme() {
+    this.isDark = !this.isDark;
+    this.className = this.isDark ? 'dark-theme' : '';
+  }
 }
